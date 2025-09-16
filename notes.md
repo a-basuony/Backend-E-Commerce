5facaded8a922c8b6ba2a2ed8ea1262a97e2da72
Ahmed Basuony <A.Basuony.social@gmail.com>
2025-09-16 21:49:22 +0300

    updating

---
+++ notes.md
@@ -0,0 +1,5 @@
+npm init -y
+create server.js file
+npm install -D nodemon
+npm install dotenv and use it
+create .gitignore file
--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 
 const app = express();
-dotenv.config({ path: "config.env" });
+dotenv.config({ path: "config.env" }); // load first ✅
+
+const NODE_ENV = process.env.NODE_ENV || "development";
+
+if (NODE_ENV === "development") {
+  app.use(morgan("dev"));
+  console.log(`mode: ${NODE_ENV}`);
+}
+
+app.get("/hello-again", (req, res) => {
+  res.send("hello, world");
+});
 
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
+  console.log("NODE_ENV is:", NODE_ENV);
+
   console.log(`server is running on port ${PORT}`);
 });
