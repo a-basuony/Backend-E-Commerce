e7858f79c326269153096b0158c2501280472034
Ahmed Basuony <A.Basuony.social@gmail.com>
2025-09-23 22:14:58 +0300

    Category CRUD operations & pagination

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,3 +1,95 @@
+const Category = require("../models/category.model");
+
 exports.getCategories = async (req, res) => {
-  res.send("getCategories");
+  try {
+    const page = +req.query.page || 1;
+    const limit = +req.query.limit || 10;
+
+    const categories = await Category.find()
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .sort({ createdAt: -1 });
+    const total = await Category.countDocuments();
+    const totalPages = Math.ceil(total / limit);
+
+    res.status(200).json({
+      message: "success",
+      data: categories,
+      pagination: {
+        total,
+        page,
+        limit,
+        totalPages: totalPages,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// read one
+exports.getCategory = async (req, res) => {
+  try {
+    const categoryId = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(categoryId)) {
+      return res.status(400).json({ message: "Invalid category ID" });
+    }
+    const category = await Category.findById(categoryId);
+    if (!category) {
+      return res.status(404).json({ message: "category not found " });
+    }
+
+    res.status(200).json({ message: "success", data: category });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// create category
+exports.createCategory = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const category = await Category.create({ name });
+    res.status(201).json({ message: "creating category", data: category });
+  } catch (error) {
+    // التعامل مع duplicate key error
+    if (error.code === 11000) {
+      return res.status(400).json({ message: "Category name must be unique" });
+    }
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// update
+exports.updateCategory = async (req, res) => {
+  try {
+    const categoryId = req.params.id;
+    const { name, slug } = req.body;
+    const category = await Category.findById(categoryId);
+    if (!category) {
+      return res.status(404).json({ message: "category not found" });
+    }
+    category.name = name;
+    await category.save();
+    res.status(200).json({
+      message: "updating category",
+      data: category,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// delete
+exports.deleteCategory = async (req, res) => {
+  try {
+    const categoryId = req.params.id;
+    const category = await Category.findByIdAndDelete(categoryId);
+    if (!category) {
+      return res.status(404).json({ message: "category not found" });
+    }
+    res.status(200).json({ message: "deleting category", data: category });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
--- routes/category.route.js
+++ routes/category.route.js
@@ -1,8 +1,18 @@
 const express = require("express");
-const { getCategories } = require("../controllers/category.controller");
+const {
+  getCategories,
+  createCategory,
+  updateCategory,
+  getCategory,
+  deleteCategory,
+} = require("../controllers/category.controller");
 
 const router = express.Router();
 
 router.get("/", getCategories);
+router.get("/:id", getCategory);
+router.post("/", createCategory);
+router.put("/:id", updateCategory);
+router.delete("/:id", deleteCategory);
 
 module.exports = router;
