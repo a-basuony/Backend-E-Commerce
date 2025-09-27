583a200de4de8b8bbe5412f0c1fa772930a64400
Ahmed Basuony <A.Basuony.social@gmail.com>
2025-09-27 13:01:07 +0300

    adding Brand model with CRUD operations

---
+++ controllers/brands.controller.js
@@ -0,0 +1,97 @@
+const slugify = require("slugify");
+const asyncHandler = require("express-async-handler");
+const BrandModel = require("../models/brand.model");
+const ApiError = require("../utils/apiError");
+
+// @desc    Get all brands
+// @route   GET /api/v1/brands
+// @access  Public
+exports.getBrands = asyncHandler(async (req, res, next) => {
+  const page = +req.query.page || 1;
+  const limit = +req.query.limit || 10;
+
+  const brands = await BrandModel.find({})
+    .skip((page - 1) * limit)
+    .limit(limit);
+  if (!brands) {
+    return next(new ApiError("Brands not found", 404));
+  }
+
+  const total = await BrandModel.countDocuments();
+  const totalPages = Math.ceil(total / limit);
+
+  res.status(200).json({
+    message: "success",
+    count: brands.length,
+    data: brands,
+    pagination: { total, page, limit, totalPages },
+  });
+});
+
+// @desc    Get brand by ID
+// @route   GET /api/v1/brands/:id
+// @access  Public
+exports.getBrand = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+
+  const brand = await BrandModel.findById(id);
+  if (!brand) {
+    return next(new ApiError(`Brand not found for id: ${id}`, 404));
+  }
+
+  res.status(200).json({ message: "success", data: brand });
+});
+
+// @desc    create brand
+// @route   POST /api/v1/brands
+// @access  Public
+exports.createBrand = asyncHandler(async (req, res, next) => {
+  const { name } = req.body;
+
+  const brand = await BrandModel.create({
+    name,
+    slug: slugify(name),
+    image: "no-image.jpg",
+  });
+  if (!brand) {
+    return next(new ApiError("Create failed : Brand not found", 404));
+  }
+
+  res.status(201).json({ message: "Brand created", data: brand });
+});
+
+// @desc    Update brand by ID
+// @route   PUT /api/v1/brands/:id
+// @access  Public
+exports.updateBrand = asyncHandler(async (req, res, next) => {
+  const { name } = req.body;
+  const { id } = req.params;
+
+  const brand = await BrandModel.findById(id);
+  if (!brand) {
+    return next(
+      new ApiError(`Update failed : Brand not found for id: ${id}`, 404)
+    );
+  }
+
+  brand.name = name || brand.name;
+  brand.slug = slugify(brand.name);
+  const updatedBrand = await brand.save();
+  res.status(200).json({ message: "Brand updated", data: updatedBrand });
+});
+
+// @desc    Delete brand by ID
+// @route   DELETE /api/v1/brands/:id
+// @access  Public
+exports.deleteBrand = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+
+  const brand = await BrandModel.findByIdAndDelete(id);
+  if (!brand) {
+    return next(
+      new ApiError(`Delete failed : Brand not found for id: ${id}`, 404)
+    );
+  }
+
+  res.status(200).json({ message: "Brand deleted", data: brand });
+});
