023e19e8c13c5550d8cfebbb961afbff24d29b96
Ahmed Basuony <A.Basuony.social@gmail.com>
2025-09-29 01:27:01 +0300

    adding product controller

---
+++ controllers/product.controller.js
@@ -0,0 +1,62 @@
+const asyncHandler = require("express-async-handler");
+
+const Product = require("../models/product.model");
+const ApiError = require("../utils/apiError");
+
+// @desc       Get all products
+// @route      GET /api/v1/products
+// @access     Public
+
+exports.getProducts = asyncHandler(async (req, res, next) => {
+  const page = +req.query.query || 1;
+  const limit = +req.query.limit || 10;
+
+  const products = await Product.find({})
+    .skip((page - 1) * limit)
+    .limit(limit);
+
+  if (!products) {
+    return next(new ApiError("Products not found", 404));
+  }
+  const total = await Product.countDocuments();
+  const totalPages = total / limit;
+
+  res.status(200).json({
+    message: "success",
+    count: products.length,
+    data: products,
+    pagination: { total, page, limit, totalPages },
+  });
+});
+
+// @desc       Get product by ID
+// @route      GET /api/v1/products/:id
+// @access     Public
+exports.getProduct = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+
+  const product = await Product.findById(id);
+  if (!product) {
+    return next(new ApiError(`Product not found for id: ${id}`, 404));
+  }
+
+  res.status(200).json({
+    message: "success",
+    data: product,
+  });
+});
+
+// @desc        Create Product
+// @route       POST /api/v1/products
+// @access      Private
+exports.createProduct = asyncHandler(async (req, res, next) => {
+  const product = await Product.create(req.body);
+  if (!product) {
+    return next(new ApiError("Create failed : Product not found", 404));
+  }
+
+  res.status(201).json({
+    message: "Product created",
+    data: product,
+  });
+});
