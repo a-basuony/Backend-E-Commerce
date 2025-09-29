68c7425469c358c283cfb340880273bb0e1314f1
Ahmed Basuony <A.Basuony.social@gmail.com>
2025-09-29 18:54:20 +0300

    adding filter and sort logic

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -8,17 +8,43 @@ const ApiError = require("../utils/apiError");
 // @access     Public
 
 exports.getProducts = asyncHandler(async (req, res, next) => {
+  // 1=> 1. filtering
+  const queryObj = { ...req.query };
+  console.log(queryObj);
+  // 2. Remove special fields (not used in filtering)
+  const excludedFields = ["page", "sort", "limit", "fields"];
+  excludedFields.forEach((el) => delete queryObj[el]);
+
+  // 3. Advanced filtering (gte, lte, gt, lt)
+  let queryStr = JSON.stringify(queryObj);
+  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+  const filters = JSON.parse(queryStr);
+
+  console.log("Filters:", queryStr);
+
+  // 2=> pagination
   const page = +req.query.page || 1;
   const limit = +req.query.limit || 10;
+  const skip = (page - 1) * limit;
 
-  const products = await Product.find({})
+  //Build query
+  const mongooseQuery = Product.find(filters)
     .populate({ path: "category", select: "name -_id" })
-    .skip((page - 1) * limit)
+    .skip(skip)
     .limit(limit);
 
-  if (products.length === 0) {
-    //we can't use `!products` here because ❌ Product.find({}) return an empty array if no products is found, not null.
+  // 3=> sorting
+  if (req.query.sort) {
+    // sorting from smallest to biggest (ascending) ?sort=price
+    // sorting from biggest to smallest (descending) ?sort=-price
+    const sortBy = req.query.sort.split(",").join(" ");
+    mongooseQuery.sort(sortBy);
+  }
+  // execute query
+  // to chain more of methods
+  const products = await mongooseQuery;
 
+  if (products.length === 0) {
     return next(new ApiError("Products not found", 404));
   }
   const total = await Product.countDocuments();
