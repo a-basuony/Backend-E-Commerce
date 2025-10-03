9c1230a3e576b9ce477407041f22b58d1a41773e
Ahmed Basuony <A.Basuony.social@gmail.com>
2025-10-03 11:36:32 +0300

    adding logic for uploading mix of images for product

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,6 +1,76 @@
+const multer = require("multer");
+const sharp = require("sharp");
+const { v4: uuid } = require("uuid");
+const path = require("path");
+const fs = require("fs");
+const asyncHandler = require("express-async-handler");
 const Product = require("../models/product.model");
 
 const factory = require("./handlersFactory");
+const ApiError = require("../utils/apiError");
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  console.log(req.body);
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new ApiError("Not an image! Please upload only images", 400), false);
+  }
+};
+
+const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
+
+exports.uploadProductImage = upload.fields([
+  { name: "imageCover", maxCount: 1 },
+  { name: "images", maxCount: 5 },
+]);
+
+exports.resizeProductImages = asyncHandler(async (req, res, next) => {
+  // if(!req.files.imageCover || !req.files.images) return next()
+  if (!req.files) return next();
+
+  // 1. folder path
+  const dir = path.join(__dirname, "../uploads/products");
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true }); // create folder if not exist
+  }
+
+  //2. coverImage
+  if (req.files.imageCover) {
+    const coverImageName = `product-cover-${uuid()}-${Date.now()}.jpeg`;
+
+    await sharp(req.files.imageCover[0].buffer)
+      .resize(2000, 1333)
+      .toFormat("jpeg")
+      .jpeg({ quality: 90 })
+      .toFile(`uploads/products/${coverImageName}`); // save to folder
+
+    req.body.imageCover = coverImageName; // save cover image name to body
+  }
+
+  //3. images
+  if (req.files.images) {
+    req.body.images = [];
+
+    await Promise.all(
+      // we use promise.all to wait for all images to be processed
+      req.files.images.map(async (file, index) => {
+        const imageName = `product-${index}-${uuid()}-${Date.now()}.jpeg`;
+        await sharp(file.buffer)
+          .resize(800, 600)
+          .toFormat("jpeg")
+          .jpeg({ quality: 90 })
+          .toFile(`uploads/products/${imageName}`); // save to folder
+
+        req.body.images.push(imageName);
+      })
+    );
+  }
+
+  next();
+});
 
 // @desc Get all products
 // @route GET /api/v1/products
