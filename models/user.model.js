468659cf6d464d478ec99f30ba23a47fd5581595
Ahmed Basuony <A.Basuony.social@gmail.com>
2025-10-04 10:13:09 +0300

    create a user model for authentication and authorization

---
+++ models/user.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Please add a user name"],
+      minlength: [3, "User name must be at least 3 characters long"],
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please add a user email"],
+      lowercase: true,
+      unique: true,
+    },
+    phone: String,
+    profileImage: String,
+    password: {
+      type: String,
+      required: [true, "Please add a user password"],
+      minlength: [6, "User password must be at least 6 characters long"],
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+); // we can add favorites
+
+module.exports = mongoose.model("User", userSchema);
