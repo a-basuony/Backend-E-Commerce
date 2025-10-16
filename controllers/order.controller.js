d63aec7991b601ef899335214161eb56cca4132a
Ahmed Basuony <A.Basuony.social@gmail.com>
2025-10-16 17:26:22 +0300

    logic for create cahs order

---
+++ controllers/order.controller.js
@@ -0,0 +1,65 @@
+const expressAsyncHandler = require("express-async-handler");
+
+const Order = require("../models/order.model");
+const Product = require("../models/product.model");
+const Cart = require("../models/cart.model");
+const ApiError = require("../utils/apiError");
+
+// @desc    create cash order
+// @route   POST /api/v1/orders/cartId
+// @access  Private / user
+exports.createCashOrder = expressAsyncHandler(async (req, res, next) => {
+  const userId = req.user._id;
+
+  //1. Get user cart
+  const cart = await Cart.findOne({ user: userId });
+  if (!cart) {
+    return next(new ApiError("Cart not found", 404));
+  }
+
+  //2. Calculate order total
+  const taxPrice = 0;
+  const shippingPrice = 0;
+
+  const totalOrderPrice = cart.totalPriceAfterDiscount
+    ? cart.totalPriceAfterDiscount + taxPrice + shippingPrice
+    : cart.totalCartPrice + taxPrice + shippingPrice;
+
+  //3. create new order
+  const order = await Order.create({
+    user: userId,
+    cartItems: cart.cartItems,
+    shippingAddress: req.body.shippingAddress,
+    totalOrderPrice: totalOrderPrice,
+  });
+
+  // 4. Decrease product quantity and increase sold after creating order, decrement product quantity, increment product sold
+  const bulkOption = cart.cartItems.map((item) => ({
+    updateOne: {
+      filter: { _id: item.productId },
+      update: { $inc: { quantity: -item.quantity, sold: +item.quantity } },
+    },
+  }));
+
+  if (cart.cartItems.length > 0) {
+    await Product.bulkWrite(bulkOption);
+  }
+  // 5. clear user cart
+  await Cart.deleteOne({ user: userId });
+
+  // 6. send response
+  res.status(201).json({
+    status: "success",
+    message: "Order created successfully",
+    data: order,
+  });
+});
+
+// {
+//   "shippingAddress": {
+//     "details": "123 Street, Downtown",
+//     "city": "Cairo",
+//     "phone": "01123456789",
+//     "postalCode": "11511"
+//   }
+// }
---
+++ models/order.model.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Order must belong to a user"],
+    },
+    cartItems: [
+      {
+        product: {
+          type: mongoose.Schema.ObjectId,
+          ref: "Product",
+        },
+        quantity: Number,
+        price: Number,
+        color: String,
+      },
+    ],
+    shippingAddress: {
+      details: { type: String, required: true },
+      city: { type: String, required: true },
+      phone: { type: String, required: true },
+      postalCode: String,
+    },
+    taxPrice: {
+      type: Number,
+      default: 0,
+    },
+    shippingPrice: {
+      type: Number,
+      default: 0,
+    },
+    totalOrderPrice: {
+      type: Number,
+    },
+    paymentMethodType: {
+      type: String,
+      enum: ["cash", "card"],
+      default: "cash",
+    },
+    isPaid: {
+      type: Boolean,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Order", orderSchema);
