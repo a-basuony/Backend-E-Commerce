20c559b6df63089ee6a8583842ca57d23c5d7c85
Ahmed Basuony <A.Basuony.social@gmail.com>
2026-02-07 23:37:00 +0200

    access control allow origin

--- server.js
+++ server.js
@@ -33,7 +33,19 @@ app.use(
   }),
 );
 
-app.options(/(.*)/, cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-CSRF-Token, X-Requested-With",
+  );
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(compression());
 app.use(cookieParser());
